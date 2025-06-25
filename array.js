d31e581b367251239d977dad2b8d810968526922
sundaymoses <sandemoses2019@gmail.com>
2025-06-25 16:14:30 +0300

    push/pop methods

---
+++ array.js
@@ -0,0 +1,10 @@
+//declareing
+let arr=[];
+//or
+// let arr2=new Array();
+// assigning
+arr.push("Apple", "Mango");
+// console.log(arr);
+arr.pop();//removes the last array element
+arr.at(-1);//returns the last element just as array.pop
+console.log(arr);
