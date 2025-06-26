b91d8ce210c30476611ffb10b7f1951976521265
sundaymoses <sandemoses2019@gmail.com>
2025-06-26 17:00:47 +0300

    learnt the filter usage

--- filter.js
+++ filter.js
@@ -1,14 +1,16 @@
+//instead of using for loop with a lot of codes, filter methods makes it easier for us in the arrays sector
+let arr=[1,49,65,43,2,3,4,5,44,55,66,5];
+//return numbers greater than 10
 
-for (let i = 1; i<=10; i++) {
-    let str= "";
-    // for (let j = i; j <=10 ; j++) {
-    //     str +="*  ";
-      
-    // }
-    for (let j = 1; j <= 10; i++) {
-         str +="*  ";
-            console.log(str);
+let bigNumbers= arr.filter((num)=>{
+    if(num>10){
+        console.log(num)
     }
-
- 
-}
+})
+//filter only returns things that satisfy a condition
+//we could do this with a for loop to see
+// for(i=1;1<=arr.length; i++){
+//    if(arr[i]>10)
+//     console.log(arr[i]);
+// }
+//they all work except that use one where need be
