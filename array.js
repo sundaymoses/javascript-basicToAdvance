e2b59dfee4485d1870507c425f816e488bd4b34e
sundaymoses <sandemoses2019@gmail.com>
2025-06-26 15:34:57 +0300

    array concat learnt

--- array.js
+++ array.js
@@ -7,4 +7,10 @@ arr.push("Apple", "Mango");
 // console.log(arr);
 arr.pop();//removes the last array element
 arr.at(-1);//returns the last element just as array.pop
-console.log(arr);
+// console.log(arr);
+let arr2=[3,4,5];
+let arr3=arr2.concat([2,4]).concat(arr)//joining since we can not split an array
+//to run a function .forEach array
+arr3.forEach((item)=>{
+console.log("hi " +item);
+})
