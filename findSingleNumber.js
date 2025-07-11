f612eaf78f56fedd1180a3950636efb2bf725443
sundaymoses <sandemoses2019@gmail.com>
2025-07-11 12:16:01 +0300

    findSingleNumber completed

---
+++ findSingleNumber.js
@@ -0,0 +1,16 @@
+function singleNum(arr) {
+    let leng;
+    let res=[];
+for (let i = 0; i < arr.length; i++) {
+  res.push(arr.filter((el)=> el === arr[i]));
+}
+res.forEach((el)=>{
+    if (el.length===1){
+   leng=el;
+    }
+
+}
+)
+return leng[0];
+}
+console.log(singleNum([1,2,1,2,4]))
